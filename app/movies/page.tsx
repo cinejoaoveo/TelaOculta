990904e2d82b951e80742b8b74a9cacec23926c2
cinejoaoveo <cinejoaoveo@gmail.com>
2025-09-23 00:26:13 -0300

    Add files via upload

---
+++ app/movies/page.tsx
@@ -0,0 +1,11 @@
+import CategoryPage from "@/app/CategoryPage";
+
+export default function MoviesPage() {
+  return (
+    <CategoryPage
+      title="Filmes"
+      mediaType="movie"
+      fetchParams={{}}
+    />
+  );
+}
---
+++ app/series/page.tsx
@@ -0,0 +1,11 @@
+import CategoryPage from "@/app/CategoryPage";
+
+export default function SeriesPage() {
+  return (
+    <CategoryPage
+      title="Séries"
+      mediaType="tv"
+      fetchParams={{}}
+    />
+  );
+}

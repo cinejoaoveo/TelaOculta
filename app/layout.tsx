2ca8572f762f0b3bfd1c225bad6d306677743c0e
cinejoaoveo <cinejoaoveo@gmail.com>
2025-09-23 00:30:08 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { Suspense } from "react"; // 1. Importar o Suspense
+import Script from "next/script"; // Importando o componente de Script
 
 export const metadata: Metadata = {
   title: "TelaOculta",
@@ -30,6 +31,26 @@ export default function RootLayout({
           </main>
           <Footer />
         </div>
+
+        {/* Script para desabilitar as ferramentas de desenvolvedor */}
+        <Script src="https://cdn.jsdelivr.net/npm/disable-devtool@latest" strategy="lazyOnload" />
+        <Script id="disable-devtool-config" strategy="lazyOnload">
+          {`
+            DisableDevtool({
+              disableMenu: true,
+              disableSelect: false,
+              disableCopy: false,
+              disableCut: true,
+              disablePaste: false,
+              clearLog: true,
+              interval: 500,
+              detectors: [0, 1, 3, 4, 5, 6, 7],
+              ondevtoolopen: function(type, next) {
+                window.location.href = 'https://i.ibb.co/5hH6bbp2/tentando-inspecionar-o-site.png';
+              }
+            });
+          `}
+        </Script>
       </body>
     </html>
   );
